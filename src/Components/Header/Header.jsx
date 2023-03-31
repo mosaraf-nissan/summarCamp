37c1743521acf915829345d5a6d16d1526b8c111
Nissan1998 <jnnissanm@gmail.com>
2023-03-31 17:01:39 +0600

    CartDataAdded

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const Header = () => {
   return (
     <div className="mx-2">
-      <div className="navbar bg-indigo-400 p-5 rounded-b-lg">
+      <div className="navbar bg-indigo-400 p-4 rounded-b-lg">
         <div className="flex-1 ">
-          <a className="btn btn-ghost text-white normal-case font-bold text-2xl">
-            Knowledge is <span className="text-5xl ml-1"> Power </span>
+          <a href="">
+            <img
+              className=" w-56 "
+              src="../../../public/images/cafe.png"
+              alt=""
+            />
           </a>
         </div>
         <div>
--- src/Components/SideCart/Cart.jsx
+++ src/Components/SideCart/Cart.jsx
@@ -5,9 +5,7 @@ const Cart = ({ data }) => {
   return (
     <div className="sticky top-0 mt-5">
       <h1 className="font-semibold">Bookmarked Blogs :- {data.length}</h1>
-      <div>
-        <h1>{data.name}</h1>
-      </div>
+      <div></div>
     </div>
   );
 };
