6417ae37c99ad14f23e2888c0b68f281b0d80e3d
Nissan1998 <jnnissanm@gmail.com>
2023-04-01 04:19:12 +0600

    Revision

--- src/Components/SideCart/Cart.jsx
+++ src/Components/SideCart/Cart.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import CartTitle from "./CartTitle/CartTitle";
 
 const Cart = ({ data, newData }) => {
-  console.log(newData, "forWWW");
+  console.log(newData, data, "forWWW");
 
   let totalReadTime = 0;
   for (const singleData of newData) {
@@ -13,11 +12,15 @@ const Cart = ({ data, newData }) => {
       <div className="w-full h-20 border-2 border-orange-500 flex justify-center align-middle items-center mb-5 bg-white rounded-lg text-blue-500 font-semibold ">
         <h1>Spent time on read :- {totalReadTime} min</h1>
       </div>
-      <h1 className="font-semibold">Bookmarked Blogs :- {data.length}</h1>
+      <h1 className="font-semibold underline">
+        Bookmarked Blogs :- {data.length}
+      </h1>
       {data.map((title) => (
-        <ol>
-          <li>{title.title}</li>
-        </ol>
+        <div className="py-1">
+          <h3 className="h-20 border-2 bg-slate-100 text-black items-center flex justify-center mb-5 mt-5 font-semibold rounded-lg">
+            {title.title}
+          </h3>
+        </div>
       ))}
     </div>
   );
