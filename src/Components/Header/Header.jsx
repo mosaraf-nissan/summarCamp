36050c0a37209a382ab5f4507ed901c6cfb0d686
Nissan1998 <jnnissanm@gmail.com>
2023-03-31 20:15:55 +0600

    Deployed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,21 +3,17 @@ import React from "react";
 const Header = () => {
   return (
     <div className="mx-2">
-      <div className="navbar bg-indigo-400 p-4 rounded-b-lg">
+      <div className="navbar bg-indigo-400 p-1 rounded-b-lg">
         <div className="flex-1 ">
           <a href="">
-            <img
-              className=" w-56 "
-              src="../../../public/images/cafe.png"
-              alt=""
-            />
+            <img className=" w-56 " src="/images/reactkk.png" alt="knowledge" />
           </a>
         </div>
         <div>
           <div>
-            <label className="btn btn-ghost btn-circle avatar">
+            <label className="btn btn-ghost btn-circle avatar border-2 border-orange-600">
               <div>
-                <img src="../../../public/images/profile8.png" />
+                <img src="/images/react.png" alt="" />
               </div>
             </label>
           </div>
